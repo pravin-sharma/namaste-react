f2cc394e48c292a214209c98e028c2a097a258fd
PRAVIN SHARMA <pravinsharma9610@gmail.com>
2023-01-29 20:57:01 +0530

    Chapter 02 - Igniting our app

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
 const heading1 = React.createElement('h1', {key:1}, 'Pravin')
 const heading2 = React.createElement('h1', {key:2}, 'Sharma')
+
 const heading = React.createElement(
     "div",
     {
@@ -9,5 +13,5 @@ const heading = React.createElement(
   );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
+console.log("dev")
 root.render(heading);
