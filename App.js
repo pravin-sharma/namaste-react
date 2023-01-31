feb394333576b3cad6804379b103f16463b7dbd9
PRAVIN SHARMA <pravinsharma9610@gmail.com>
2023-01-31 14:03:39 +0530

    added babel for removing console

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-const heading1 = React.createElement('h1', {key:1}, 'Pravin')
-const heading2 = React.createElement('h1', {key:2}, 'Sharma')
+const heading1 = React.createElement('h1', {}, 'Pravin')
+const heading2 = React.createElement('h1', {}, 'Sharma')
 
 const heading = React.createElement(
     "div",
     {
       id: "header"
     },
-    [heading1, heading2]
+    [React.createElement('h1', {}, 'Pravin'),React.createElement('h1', {}, 'Sharma')]
   );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log("dev")
+
 root.render(heading);
+
+//remove console log using babel
+// keys in sibling elements
